ed15fe2647486e2bc7f215ac0b1eb3a391486de8
itlemon <lemon_jiang@aliyun.com>
2022-04-27 23:10:25 +0800

    MOD: 优化SEO

---
+++ docs/.vuepress/public/js/toutiao.js
@@ -0,0 +1,7 @@
+(function () {
+    const el = document.createElement("script");
+    el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?ba17d68774771bfdb932beaab9892091e6a714eeebce32b9a63fe55be2f4a7b5fd9a9dcb5ced4d7780eb6f3bbd089073c2a6d54440560d63862bbf4ec01bba3a";
+    el.id = "ttzz";
+    const s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(el, s);
+})(window)
