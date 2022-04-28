da8d9a5eaaae0b4a5d555e6788d8405563fb006d
itlemon <lemon_jiang@aliyun.com>
2022-04-28 22:53:26 +0800

    MOD: 优化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # author: itlemon
 
 git pull
-kill -9 $(ps aux | grep yarn | tr -s ' '| cut -d ' ' -f 2)
+kill -9 $(ps aux | grep yarn | tr -s ' '| cut -d ' ' -f 2 | sed -n '1p')
 yarn install
 yarn build
 yarn serve &
