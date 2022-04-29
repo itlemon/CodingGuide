0d90bd9786a2b3ca325f1a1dae60035cbfbe78a7
itlemon <lemon_jiang@aliyun.com>
2022-04-29 11:44:35 +0800

    MOD: 测试代码复制插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ import {pwaPlugin} from '@vuepress/plugin-pwa'
 import {pwaPopupPlugin} from '@vuepress/plugin-pwa-popup'
 import {nprogressPlugin} from '@vuepress/plugin-nprogress'
 import {gitPlugin} from '@vuepress/plugin-git'
-// import {copyCode} from "vuepress-plugin-copy-code2"
+import {copyCode} from "vuepress-plugin-copy-code2"
 import {navbar, sidebar} from './configs'
 
 export default defineUserConfig({
@@ -203,18 +203,13 @@ export default defineUserConfig({
             contributors: false
         }),
         // 代码复制插件
-        // copyCode({
-        //     duration: 2000,
-        //     showInMobile: true,
-        //     // locales: {
-        //     //     copy: '复制代码',
-        //     //     hint: '复制成功!'
-        //     // }
-        // }),
-        [
-            "vuepress-plugin-clipboard", {
-            successText: '拷贝成功!'
-            }
-        ]
+        copyCode({
+            duration: 2000,
+            showInMobile: true,
+            // locales: {
+            //     copy: '复制代码',
+            //     hint: '复制成功!'
+            // }
+        }),
     ]
 })
