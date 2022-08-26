0a3e163ff6f04ce5f75032982a547d26277c7a4f
itlemon <lemon_jiang@aliyun.com>
2022-08-27 00:56:57 +0800

    MOD: 修复__dirname问题

--- docs/.vuepress/theme/test.js
+++
@@ -1,9 +0,0 @@
-import {fileURLToPath} from 'url';
-import {path} from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
-    __dirname
-}
