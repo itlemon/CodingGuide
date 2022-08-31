cf5a4bf56e4d627f90024f33b71b49fcbf7602f5
itlemon <lemon_jiang@aliyun.com>
2022-08-31 22:23:32 +0800

    MOD: 升级到2.0.0.51

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,6 @@
 import { defineClientConfig } from "@vuepress/client";
 import Layout from "./layouts/Layout.vue";
+import NotFound from "./layouts/NotFound.vue";
 
 export default defineClientConfig({
     layouts: {
