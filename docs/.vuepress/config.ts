8b6ebce4cc32c205561e2a88de28fd8d4759c6a1
itlemon <lemon_jiang@aliyun.com>
2022-08-31 22:25:13 +0800

    MOD: 升级到2.0.0.51

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,4 @@
-import {defineUserConfig} from 'vuepress'
+import {defineUserConfig} from "@vuepress/cli";
 import {defaultTheme} from "@vuepress/theme-default";
 import {docsearchPlugin} from '@vuepress/plugin-docsearch'
 import {googleAnalyticsPlugin} from '@vuepress/plugin-google-analytics'
