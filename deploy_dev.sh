106416186cf6e6cedbe7da4de2340d745f0d73e3
itlemon <lemon_jiang@aliyun.com>
2022-10-08 20:11:07 +0800

    MOD: 添加dev脚本

---
+++ deploy_dev.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# author: itlemon
+
+port=4000
+
+function get_pid_by_listen_port() {
+        pattern_str="*:$port\\b"
+        pid=$(ss -n -t -l -p | grep "$pattern_str" | column -t | awk -F ',' '{print $(NF-1)}')
+
+        # 当版本号为 "ss utility, iproute2-ss161009" 时, ss 命令输出格式为:
+        #              LISTEN  0  5  *:8000  *:*  users:(("python2.7",pid=7130,fd=3))
+        # 此时需要进一步处理, 只获取进程 PID 值.
+        [[ $pid =~ "pid" ]] && pid=$(echo $pid | awk -F '=' '{print $NF}')
+
+        echo $pid
+}
+
+pid=$(get_pid_by_listen_port $port)
+if [ -n "$pid" ]
+then
+        echo "find pid: $pid, kill it..."
+        kill $pid
+else
+        echo 'cannot find listened port: '$port
+fi
+
+git fetch --all && git reset --hard origin/dev && git pull
+sudo npm install
+sudo npm run docs:build
+sudo nohup npm run docs:serve &
