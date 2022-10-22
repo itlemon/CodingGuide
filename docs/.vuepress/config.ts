e1d8cea8c477f290cce89d1eda32f124b78a24c4
itlemon <lemon_jiang@aliyun.com>
2022-10-23 01:23:06 +0800

    MOD: 重新开始

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import {defineUserConfig} from '@vuepress/cli';
 import {defaultTheme} from '@vuepress/theme-default'
 import {googleAnalyticsPlugin} from '@vuepress/plugin-google-analytics'
-import {docsearchPlugin} from '@vuepress/plugin-docsearch'
+// import {docsearchPlugin} from '@vuepress/plugin-docsearch'
 import {pwaPlugin} from '@vuepress/plugin-pwa'
 import {pwaPopupPlugin} from '@vuepress/plugin-pwa-popup'
 import {gitPlugin} from '@vuepress/plugin-git'
@@ -81,57 +81,57 @@ export default defineUserConfig({
         }),
 
         // 全文搜索
-        docsearchPlugin({
-            appId: 'WSOVOADVOS',
-            apiKey: '8db851c8c01ec47d3598e5e575276de6',
-            indexName: 'codingguide',
-            searchParameters: {
-                facetFilters: ['tags:v2'],
-            },
-            locales: {
-                '/': {
-                    placeholder: '搜索文档',
-                    translations: {
-                        button: {
-                            buttonText: '搜索文档',
-                            buttonAriaLabel: '搜索文档',
-                        },
-                        modal: {
-                            searchBox: {
-                                resetButtonTitle: '清除查询条件',
-                                resetButtonAriaLabel: '清除查询条件',
-                                cancelButtonText: '取消',
-                                cancelButtonAriaLabel: '取消',
-                            },
-                            startScreen: {
-                                recentSearchesTitle: '搜索历史',
-                                noRecentSearchesText: '没有搜索历史',
-                                saveRecentSearchButtonTitle: '保存至搜索历史',
-                                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                                favoriteSearchesTitle: '收藏',
-                                removeFavoriteSearchButtonTitle: '从收藏中移除',
-                            },
-                            errorScreen: {
-                                titleText: '无法获取结果',
-                                helpText: '你可能需要检查你的网络连接',
-                            },
-                            footer: {
-                                selectText: '选择',
-                                navigateText: '切换',
-                                closeText: '关闭',
-                                searchByText: '搜索提供者',
-                            },
-                            noResultsScreen: {
-                                noResultsText: '无法找到相关结果',
-                                suggestedQueryText: '你可以尝试查询',
-                                reportMissingResultsText: '你认为该查询应该有结果？',
-                                reportMissingResultsLinkText: '点击反馈',
-                            },
-                        },
-                    },
-                },
-            },
-        }),
+        // docsearchPlugin({
+        //     appId: 'WSOVOADVOS',
+        //     apiKey: '8db851c8c01ec47d3598e5e575276de6',
+        //     indexName: 'codingguide',
+        //     searchParameters: {
+        //         facetFilters: ['tags:v2'],
+        //     },
+        //     locales: {
+        //         '/': {
+        //             placeholder: '搜索文档',
+        //             translations: {
+        //                 button: {
+        //                     buttonText: '搜索文档',
+        //                     buttonAriaLabel: '搜索文档',
+        //                 },
+        //                 modal: {
+        //                     searchBox: {
+        //                         resetButtonTitle: '清除查询条件',
+        //                         resetButtonAriaLabel: '清除查询条件',
+        //                         cancelButtonText: '取消',
+        //                         cancelButtonAriaLabel: '取消',
+        //                     },
+        //                     startScreen: {
+        //                         recentSearchesTitle: '搜索历史',
+        //                         noRecentSearchesText: '没有搜索历史',
+        //                         saveRecentSearchButtonTitle: '保存至搜索历史',
+        //                         removeRecentSearchButtonTitle: '从搜索历史中移除',
+        //                         favoriteSearchesTitle: '收藏',
+        //                         removeFavoriteSearchButtonTitle: '从收藏中移除',
+        //                     },
+        //                     errorScreen: {
+        //                         titleText: '无法获取结果',
+        //                         helpText: '你可能需要检查你的网络连接',
+        //                     },
+        //                     footer: {
+        //                         selectText: '选择',
+        //                         navigateText: '切换',
+        //                         closeText: '关闭',
+        //                         searchByText: '搜索提供者',
+        //                     },
+        //                     noResultsScreen: {
+        //                         noResultsText: '无法找到相关结果',
+        //                         suggestedQueryText: '你可以尝试查询',
+        //                         reportMissingResultsText: '你认为该查询应该有结果？',
+        //                         reportMissingResultsLinkText: '点击反馈',
+        //                     },
+        //                 },
+        //             },
+        //         },
+        //     },
+        // }),
 
         pwaPlugin({}),
         pwaPopupPlugin({
