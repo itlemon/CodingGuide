ce7a7fb690274e5fc316c8e583f467aed58f6bf4
itlemon <lemon_jiang@aliyun.com>
2022-10-23 02:11:30 +0800

    MOD: 重新开始

--- docs/.vuepress/public/js/baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?95441a1288ba1c91d088a118a4b4ddbb";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
