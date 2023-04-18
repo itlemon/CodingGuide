a7e339c50cce6605d52ed9c849b1dd5e10531f72
itlemon <lemon_jiang@aliyun.com>
2023-04-18 11:14:45 +0800

    mod: 修改启动脚本

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,21 @@
 #! /bin/bash
 # author: itlemon
 
-port=4000
+portArg=4000
 
 # 通过ss的方式获取pid
 function get_pid_by_listen_port() {
-        pattern_str="*:$port\\b"
-        pid=$(ss -n -t -l -p | grep "$pattern_str" | column -t | awk -F ',' '{print $(NF-1)}')
-
-        # 当版本号为 "ss utility, iproute2-ss161009" 时, ss 命令输出格式为:
-        #              LISTEN  0  5  *:8000  *:*  users:(("python2.7",pid=7130,fd=3))
-        # 此时需要进一步处理, 只获取进程 PID 值.
-        [[ $pid =~ "pid" ]] && pid=$(echo $pid | awk -F '=' '{print $NF}')
-
-        echo $pid
+  local port="$1"
+  lsof -t -i :"$port"
 }
 
-# 通过netstat的方式获取pid
-function get_pid_by_listen_port2() {
-    pid=$(netstat -nlp | grep $port | awk '{print $7}' | grep -o -E '[0-9]+')
-    echo $pid
-}
-
-pid=$(get_pid_by_listen_port $port)
+pid=$(get_pid_by_listen_port $portArg)
 if [ -n "$pid" ]
 then
         echo "find pid: $pid, kill it..."
         kill $pid
 else
-        echo 'cannot find listened port: '$port
+        echo 'cannot find listened port: '$portArg
 fi
 
 if git rev-parse --verify master
